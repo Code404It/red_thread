525d7b64ba8e0eab297880d42d91a1a0d57cbea8
code404it <jessikahd.smith@gmail.com>
2020-07-19 17:23:39 -0400

    current user concern and more

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
-class SessoinsController < ApplicationController
+class SessionsController < ApplicationController
+    include CurrentUserConcern
     def create
         user = User
         .find_by(email: params["user"]["email"])
         .try(:authenticate, params["user"]["password"])
 
         if user
-            session[:user_id] = user_id
+            session[:user_id] = user.id
             render json: {
                 status: :created, 
                 logged_in: true,
@@ -15,4 +16,23 @@ class SessoinsController < ApplicationController
             render json: {status: 401}
         end
     end
+        def logged_in
+            if @current_user
+            render json: {
+            logged_in: true 
+            user: @current_user
+            }
+        else
+            render json: {
+            logged_in: false
+            }
+        end
+    end
+        def logout
+          reset_session
+          render json: {status: 200}
+        end
+
 end
+
+
